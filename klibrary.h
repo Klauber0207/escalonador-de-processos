52ba115e84736ec301344be71b088b32ebb27316
Antônio Carlos <jhunny.tec@gmaill.com>
2021-06-26 19:45:49 -0300

    Adicionando funções.

--- klibrary.h
+++ klibrary.h
@@ -9,9 +9,37 @@ void hl(){
 	printf("Hello world");
 }
 
-/*Soma*/
 
-int soma(int a, int b){
+/*Estrutura Processo*/
+typedef struct processo{
+	int id;
+	int duracao;
+}Processo;
+
+
+/*Cria um processo com id = 0 e duração = 0.*/
+Processo cria_Processo(){
+	Processo p;
 	
-	return a+b;
+	p.id = 0;
+	p.duracao = 0;
+	return p;	
 }
+
+
+/*Imprime as informações de um processo, como id e duração.*/
+void imprime_Processo(Processo p){
+	printf("Processo %d -> tamanho: %d", p.id, p.duracao);
+	
+}
+
+
+/*Cria um Processo e atribui valores de id e duração a um processo*/
+Processo atribui_Processo(int id, int duracao){
+	Processo p;
+	p.id = id;
+	p.duracao = duracao;
+	
+	return p;
+}
+
--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include "klibrary.h"
 
 void main(){
 	
-	printf("Fala Kalleb");
+	int i = 1;
+	int t = 5;
+	Processo p = atribui_Processo(i, t);;
+	
+	imprime_Processo(p);
+	
 }
